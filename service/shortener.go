fdc86ee9fd753816907755a908a5306115d8ed16
Murid Quorishi <fazilq22@gmail.com>
2026-04-09 13:43:43 -0400

    Initial commit

--- service/shortener.go
+++ service/shortener.go
@@ -1,1 +1,51 @@
 package service
+
+import (
+	"math/rand"
+	"time"
+	"Go_URL_Shortener_CSC325/storage"
+)
+
+const charset = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789"
+
+type ShortenerService struct {
+	store *storage.MemoryStore
+}
+
+func NewShortenerService() *ShortenerService {
+	return &ShortenerService{
+		store: storage.NewMemoryStore(),
+	}
+}
+
+// Generate random short code
+func generateCode(length int) string {
+	rand.Seed(time.Now().UnixNano())
+	code := make([]byte, length)
+
+	for i := range code {
+		code[i] = charset[rand.Intn(len(charset))]
+	}
+
+	return string(code)
+}
+
+func (s *ShortenerService) Shorten(url string) string {
+	code := generateCode(6)
+
+	// Ensure uniqueness
+	for {
+		_, exists := s.store.Get(code)
+		if !exists {
+			break
+		}
+		code = generateCode(6)
+	}
+
+	s.store.Save(code, url)
+	return code
+}
+
+func (s *ShortenerService) GetOriginalURL(code string) (string, bool) {
+	return s.store.Get(code)
+}
--- storage/memory.go
+++ storage/memory.go
@@ -1,1 +1,29 @@
 package storage
+
+import "sync"
+
+type MemoryStore struct {
+	data map[string]string
+	mu   sync.RWMutex
+}
+
+func NewMemoryStore() *MemoryStore {
+	return &MemoryStore{
+		data: make(map[string]string),
+	}
+}
+
+func (m *MemoryStore) Save(code string, url string) {
+	m.mu.Lock()
+	defer m.mu.Unlock()
+
+	m.data[code] = url
+}
+
+func (m *MemoryStore) Get(code string) (string, bool) {
+	m.mu.RLock()
+	defer m.mu.RUnlock()
+
+	url, exists := m.data[code]
+	return url, exists
+}
